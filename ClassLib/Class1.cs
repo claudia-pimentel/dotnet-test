fb36bea242684516c61230dc75f1fb1d33a40e8d
Flavio Araki <f.tachikawa.araki@avanade.com>
2019-01-21 12:05:00 -0200

    Initial commit

---
+++ ClassLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLib
+{
+    public class Class1
+    {
+        public int Double(int number) 
+        {
+            throw new NotImplementedException("Please create a test first");
+        } 
+    }
+}
